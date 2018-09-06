21c3fc5d429a33020ef6be7815915354c64a0dcd
Preeti <preeti.agrawal@onjection.com>
2018-09-06 11:52:37 +0530

    sql updated

--- node (1).sql
+++ node (1).sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 27, 2018 at 07:01 AM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Host: localhost
+-- Generation Time: Sep 06, 2018 at 11:51 AM
+-- Server version: 5.7.23-0ubuntu0.16.04.1
+-- PHP Version: 7.2.9-1+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -39,13 +37,6 @@ CREATE TABLE `profile` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `profile`
---
-
-INSERT INTO `profile` (`id`, `user_id`, `profile_image`, `cover_image`, `upload_image`, `current_profile`, `current_cover`, `created_at`) VALUES
-(1, 1, '/1Ram Pratap/download.jpg,/1Ram Pratap/girl-dandelion-yellow-flowers-160699.jpeg,/1Ram Pratap/download.jpg,/1Ram Pratap//1Ram Pratap/download.jpg,/1Ram Pratap/girl-dandelion-yellow-flowers-160699.jpeg,/1Ram Pratap/download.jpg', ',/1Ram Pratap/milky-way-2695569_960_720.jpg', '/1Ram Pratap/download.jpg,/1Ram Pratap/girl-dandelion-yellow-flowers-160699.jpeg,/1Ram Pratap/download.jpg,/1Ram Pratap/girl-dandelion-yellow-flowers-160699.jpeg,/1Ram Pratap/milky-way-2695569_960_720.jpg', '/1Ram Pratap/girl-dandelion-yellow-flowers-160699.jpeg', '/1Ram Pratap/milky-way-2695569_960_720.jpg', '2018-08-25 18:39:32');
-
 -- --------------------------------------------------------
 
 --
@@ -61,7 +52,6 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `gender` varchar(30) NOT NULL,
-  `image` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -69,11 +59,20 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user_id`, `first_name`, `last_name`, `mob_no`, `email`, `password`, `gender`, `image`, `created_at`) VALUES
-(1, 1, 'Ram Pratap', 'Singh', '9451201095', 'rpdhakrey949@gmail.com', 'ram123.4', 'male', '', '2018-08-25 08:53:29'),
-(2, 2, 'jagmohan', 'Singh', '8888584854', 'ram@gmail.com', '12345', 'male', '', '2018-08-25 08:53:34'),
-(3, 3, 'Anjali', 'Singh', '9876543210', 'anjali@gmail.com', '12345', 'female', '', '2018-08-25 08:53:37'),
-(4, 4, 'ranu', 'Singh', '9867676765', 'anjali@gmail.com', '12345', 'male', '', '2018-08-25 08:55:25');
+INSERT INTO `users` (`id`, `user_id`, `first_name`, `last_name`, `mob_no`, `email`, `password`, `gender`, `created_at`) VALUES
+(1, 1, 'Swati', 'ss', '08585909577', 'swati123@gmail.com', 'admin', 'male', '2018-08-29 12:17:08'),
+(2, 2, 'Swati', 'sinha', '8585909577', 'swati123@gmail.com', 'admin', 'female', '2018-08-29 12:18:16'),
+(3, 3, 'Swati', 'sinha', '8585909577', 'sinha@gmail.com', 'admin', 'female', '2018-08-31 05:22:37'),
+(4, 4, 'dimple', 'mittal', '9898565623', 'dimple@gmail.com', 'admin', 'female', '2018-08-31 06:15:10'),
+(14, 14, 'khushi', 'priya', '8585909577', 'kk@gmail.com', 'admin', 'female', '2018-08-31 07:36:53'),
+(24, 24, 'Swati', 'sinha', '8585909577', 'sinha@gmail.com', 'admin', 'female', '2018-09-03 05:26:34'),
+(25, 25, 'suraj', 'gfgfdgfg', '8585909577', 'suraj@gmail.com', 'admin', 'male', '2018-09-05 11:06:40'),
+(26, 26, 'sneha', 'ddd', '8585909577', 'sinha@gmail.com', 'admin', 'female', '2018-09-04 13:22:32'),
+(27, 27, 'Swati', 'sinha', '8585909577', 'test@gmail.com', 'admin', 'female', '2018-09-05 05:39:21'),
+(28, 28, 'ss', 'ss', '8585909589', 'test@gmail.com', 'admin', 'male', '2018-09-05 07:22:08'),
+(29, 29, 'hina', 'srivastva', '8585909580', 'sri@gmail.com', 'admin', 'female', '2018-09-05 11:01:37'),
+(31, 31, 'gtf', 'dvgdg', '7585909577', 'uist@gmail.com', 'admin', 'male', '2018-09-05 11:10:58'),
+(32, 32, 'Swati', 'sinha', '8585909577', 'test@gmail.com', 'admin', 'female', '2018-09-05 11:16:00');
 
 --
 -- Indexes for dumped tables
@@ -99,15 +98,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
